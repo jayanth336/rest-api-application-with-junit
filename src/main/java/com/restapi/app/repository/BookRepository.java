e7bed2042b6f448d4c78637fca246c13e1e51e13
Jayanth Ramanathan <jayanthram1320@gmail.com>
2023-08-13 20:53:03 +0530

    add BookRepository.java repository class

---
+++ src/main/java/com/restapi/app/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.restapi.app.repository;
+
+import com.restapi.app.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
